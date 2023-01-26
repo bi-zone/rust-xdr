6bcccd032f708ae4408b234f211cb4fd730e7cef
qthree <qthree3@gmail.com>
2023-01-26 18:20:49 +0700

    Parse xdr block comments as rust doc comments (#3)
    * Support comments on struct fields and enum variants.
    
    * Add new example that uses xdrgen::generate_pretty

---
+++ xdrgen/examples/pretty/src/main.rs
@@ -0,0 +1,36 @@
+use xdr_codec;
+
+use std::io::Cursor;
+use xdr_codec::{unpack,pack};
+
+#[path ="../generated/simple_xdr.rs"]
+mod simple;
+
+fn main() {
+    let bar = simple::Bar { data: vec![1,2,3] };
+    let foo = simple::Foo {
+        a: 1, b: 2, c: 3,
+        bar: vec![bar.clone()],
+        bar_pair: simple::BarPair([bar.clone(), bar.clone()]),
+        barish: None,
+        name: String::from("foox"),
+        thing: simple::Things::C,
+        type_: 123,
+    };
+    // "derive_serde" feature makes this working
+    // println!("Serialized JSON: {}", serde_json::to_string(&foo).unwrap());
+
+    let mut buf = Vec::new();
+
+    pack(&foo, &mut buf).unwrap();
+    println!("foo={:?}", foo);
+    println!("buf={:?} len={}", buf, buf.len());
+
+    let mut cur = Cursor::new(buf);
+    
+    let foo2 = unpack(&mut cur).unwrap();
+
+    println!("foo={:?}", foo);
+    println!("foo2={:?}", foo2);
+    assert_eq!(foo, foo2);
+}
